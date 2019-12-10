8cc14ffa723a813528bad12b5ffdf9d86b41ba14
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-10 16:03:03 +0900

    ✨fix): module

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@
 
 // Import modules
 import Express from 'express';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import path from 'path';
@@ -11,16 +9,12 @@ import path from 'path';
 // Create application
 const app = Express();
 
-// App settings
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
 app.use(Express.static(path.join(__dirname, 'public')));
 
 // Routes settings
-import router from './src/routes';
+import router from './routes';
 app.use('/', router);
 
 app.get('/', (req, res) => {
